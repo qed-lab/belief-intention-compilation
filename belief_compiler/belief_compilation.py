124217a158fa53a3bd70694d91d7e789c6e7a67f
matthewlchristensen <mattswimftw@gmail.com>
2020-02-17 19:02:21 -0700

    continuing belief implementation

--- belief_compiler/belief_compilation.py
+++ belief_compiler/belief_compilation.py
@@ -1,4 +1,5 @@
 import Domain
+import fluenttree
 
 class BeliefCompiledProblem():
     def __init__(self, domain, problem):
@@ -6,16 +7,24 @@ class BeliefCompiledProblem():
         self.base_problem = problem
         self.initial_state = self.find_init_state()
 
+
+
     def find_init_state(self):
         state = []
-        for pred in self.problem.init_state:
+        for pred in [fluenttree.AbstractPredicate(c) for c in self.base_problem.init_state]:
             if pred.is_belief:
-                agent = pred.parameters[0]
-                
+                pred = fluenttree.AbstractPredicate(f"believes-{pred.identifier} {' '.join(pred.parameters)}")
             state.append(pred)
 
 
         return 2
+
+    def compiled_problem(self):
+        pass
+
+    def compiled_domain(self):
+        pass
+
 
 
 
