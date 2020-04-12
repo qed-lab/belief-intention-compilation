3ba4972de48892460436b5963bba2f7109adea1f
matthewlchristensen <mattswimftw@gmail.com>
2020-04-12 12:04:41 -0600

    Continued work on decompilation

--- belief_compiler/decompile_glaive.py
+++ belief_compiler/decompile_glaive.py
@@ -1,6 +1,7 @@
 from pddl.PDDLPart import PDDLPart
 from pddl.Domain import Domain
 from pddl import Utils
+import Math
 
 
 # TODO: This whole decomp process
@@ -9,12 +10,24 @@ def get_steps(plan):
     steps = pddl.children[0].children[2].children
     return steps
 
+
 def decompile(plan_str, original_domain_file):
     steps = get_steps(plan_str)
     dom_string = Utils.get_trimmed_string_from_file(original_domain_file)
     dom_child, _ = Utils.find_child(dom_string)
     orig_dom = Domain(dom_child)
     orig_actions = orig_dom.actions
+    action_names = [x.name for x in orig_actions]
+    decompiled_plan = []
     for step in steps:
-        pass
+        if ":untaken" in step.string:
+            continue
+        trimmed_name = step.string[:Math.max(step.find("_success"), step.find("_fail"))]
+        trimmed_params = 2
+        int_str = trimmed_name + str(trimmed_params)
+        if trimmed_name in action_names:
+            decompiled_plan.append(int_str)
+        else:
+            pass
+
     return plan_str
